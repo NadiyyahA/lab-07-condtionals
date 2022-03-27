d63b2f5046891a9cf248ed98b030d0cd0f281b2b
Nadiyyah Ahmed <99215113+NadiyyahA@users.noreply.github.com>
2022-03-27 01:07:29 -0400

    Completed reminders.v02.py

---
+++ reminders.v02.py
@@ -0,0 +1,27 @@
+import datetime
+
+ now = datetime.datetime.now()
+  print(now.hour)
+
+usertime = now.hour # set usertime variable to the current hour
+
+if usertime <== 5:
+    print("It's early. You should be sleeping!")
+elif usertime <== 7:
+    print ("Wake up, brew that coffee, get that mile run, get that breakfast..")
+elif usertime <== 9:
+    print ("Time to go to work.")
+elif usertime <==12:
+    print("YOU SHOULD BE WORKING!")
+elif usertime <== 13:
+    print ("Take your lunch break")
+elif usertime <== 17:
+    print ("Do you feel that afternoon lull?")
+elif usertime <== 19:
+    print ("Time to hit the gym..")
+elif usertime <== 21:
+    print ("Gotta eat that dinner.")
+elif usertime <== 23:
+    print("Get that sleep and repeat!")
+else:
+    print ("You didnt type an acceptable value (0-23)!")
